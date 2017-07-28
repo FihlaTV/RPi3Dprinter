07cd959ceb9e1c4a3f6475da54dae6b897a73376
Vilius Kraujutis <viliusk@gmail.com>
2017-07-28 09:03:55 +0300

    Automatically generated Android app module

---
+++ phone/src/main/java/lt/andro/rpi3dprinter/phone/MainActivity.kt
@@ -0,0 +1,12 @@
+package lt.andro.rpi3dprinter.phone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
